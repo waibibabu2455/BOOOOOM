3462fdfa66831f9e95c48e61c8327b66c0c67c94
Lorling <1152541346@qq.com>
2023-10-02 23:37:20 +0800

    1

--- Assets/Scripts/Lorling/UI/Text/Consume.cs
+++ Assets/Scripts/Lorling/UI/Text/Consume.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Consume : TextUIBase
+﻿public class Consume : TextUIBase
 {
     protected override void DateUpdate()
     {
