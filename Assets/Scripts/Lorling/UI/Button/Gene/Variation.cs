1cad79ab413ad42e500f327296b614a0e2197570
Zuoliang Liao <zuoliangliao@gmail.com>
2023-10-03 14:55:17 +0800

    variation

---
+++ Assets/Scripts/Lorling/UI/Button/Gene/Variation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variation : ButtonUIBase
+{
+    public override void Click()
+    {
+        sure.GetComponent<Sure>().SetAction(Func);
+    }
+
+    void Func()
+    { Organ neworgan = machine.OrganPool[Random.Range(0,machine.OrganPool.Count-1)];
+        if (PlayerAttribute.Instance.organList.Contains(neworgan) == false)
+        {
+            PlayerAttribute.Instance.organList.Add(neworgan);
+            neworgan.enable = true;
+            neworgan.OrganEffect();
+            Debug.Log(neworgan.OrganName);
+        }
+        Debug.Log("failed to get a new organ");
+
+    }
+}
