9cb6d90486e5130fbc2e5fbf8f7853da4002b503
Lorling <1152541346@qq.com>
2023-10-10 00:16:17 +0800

    1

---
+++ Assets/Scripts/Lorling/UI/Text/suojin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class suojin : MonoBehaviour
+{
+    Text text;
+    private void Awake()
+    {
+        text = GetComponent<Text>();
+        string s = text.text;
+        text.text = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0" + s;
+    }
+}
